57f87ed4e315ee2b7141446a75f2f57fe7e6d42b
leandrolivera <leandro.olivera03@gmail.com>
2021-01-28 21:54:13 -0300

    Map Activity andando, falta agregar funcionalidades.

--- app/src/main/java/com/example/farmatom/AltaOrdenActivity.java
+++ app/src/main/java/com/example/farmatom/AltaOrdenActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
@@ -29,6 +30,7 @@ import androidx.room.Room;
 import com.example.farmatom.Model.Medicamento;
 import com.example.farmatom.Model.Orden;
 import com.example.farmatom.Room.Orden.AppDatabase;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Objects;
 
@@ -52,6 +54,7 @@ public class AltaOrdenActivity extends AppCompatActivity {
         setContentView(R.layout.alta_orden);
         Toolbar toolbar = findViewById(R.id.toolbarHome);
         setSupportActionBar(toolbar);
+        final Button botonAgregarUbicacion = findViewById(R.id.botonUbicacion);
         final Button botonAgregarMedicamento = findViewById(R.id.listadoMedicamentos);
         final Button botonGuardarOrden = findViewById(R.id.botonGuardarOrden);
 
@@ -62,6 +65,31 @@ public class AltaOrdenActivity extends AppCompatActivity {
         nombreMedicamento = (TextView) findViewById(R.id.nombreMedicamento);
         totalNuevoPedido = (TextView) findViewById(R.id.totalNuevoPedido);
         progressBar1 = (ProgressBar) findViewById(R.id.progressBar1);
+
+        botonEnvioPedido.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                botonAgregarUbicacion.setEnabled(botonEnvioPedido.isChecked());
+            }
+        });
+
+        botonTakeawayPedido.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                direccionPedidoNuevo.setEnabled(true);
+                direccionPedidoNuevo.setText("");
+            }
+        });
+
+        botonAgregarUbicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AltaOrdenActivity.this, MapActivity.class);
+                LatLng ubicacion = new LatLng(0,0);
+                i.putExtra("ubicacion",ubicacion);
+                startActivityForResult(i, CODIGO_ACTIVIDAD);
+            }
+        });
 
         botonAgregarMedicamento.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/farmatom/MapActivity.java
@@ -0,0 +1,29 @@
+package com.example.farmatom;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private GoogleMap myMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        myMap=googleMap;
+        // COMPLETAR
+    }
+}
